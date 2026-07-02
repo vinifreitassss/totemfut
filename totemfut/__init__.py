d4d97d667060afc873676b67ddeb4f84be82e68d
vinifreitassss <viniciusfreitas994@gmail.com>
2026-07-02 09:42:31 -0300

    feat: export v3 generator

--- totemfut/__init__.py
+++ totemfut/__init__.py
@@ -1,4 +1,7 @@
-from .v2 import GeneratedTotem, TotemConfig, generate_totem_svg, safe_filename
+from totemfut.v3 import GeneratedTotem
+from totemfut.v3 import TotemConfig
+from totemfut.v3 import generate_totem_svg
+from totemfut.v3 import safe_filename
 
 __all__ = [
     "GeneratedTotem",
